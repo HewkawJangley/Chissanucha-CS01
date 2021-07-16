b1928fbe3160ae1f7582f687a8f0fa2bf423051d
HewkawJangley <ikkyu1609@gmail.com>
2021-07-16 09:50:36 +0700

    Create CS01-7.py

---
+++ CS01-7.py
@@ -0,0 +1,23 @@
+A=int(input("Enter:"))
+if(A<=10):
+    print("ไม่ผ่าน")
+elif(A<=20):
+    print("ปรับปรุง")
+else :
+    print("ผ่าน")
+A=int(input("Enter:"))
+if(A<=10):
+    print("ไม่ผ่าน")
+elif(A<=20):
+    print("ปรับปรุง")
+else :
+    print("ผ่าน")
+A=int(input("Enter:"))
+if(A<=10):
+    print("ไม่ผ่าน")
+elif(A<=20):
+    print("ปรับปรุง")
+else :
+    print("ผ่าน")
+
+#for index in range(3): ผมใช้อันนี้ละมันไม่ได้อะคับ
