e8589c4537f6024e8bdbff4b089230896e923e75
HewkawJangley <ikkyu1609@gmail.com>
2021-07-30 11:30:03 +0700

    update

--- CS01-Max Min.py
+++ CS01-Max Min.py
@@ -1,7 +1,10 @@
-Num = int(input("Exter loop:"))
-NumToTal=[]
+Num = int(input('Enter Your Loop: '))
+NumTotal=[]
 for i in range(Num):
-    data = int(input("Exter data:"))
-    NumToTal += [data]
-NumToTal.sort()
-print()
+    data = int(input('Enter your Data: '))
+    NumTotal += [data]
+    print(NumTotal)
+NumTotal.sort(reverse=False)
+print(NumTotal[0])
+nvm = len(NumTotal)
+print(NumTotal[nvm-1])
